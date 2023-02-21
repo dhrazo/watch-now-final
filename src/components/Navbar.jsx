7ef61e361426ee42bea0e6af59d2619a9664a377
David <rrazodavid@gmail.com>
2023-02-21 13:56:44 -0800

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utilities/constants";
@@ -9,6 +9,12 @@ const Navbar = () => (
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <Box>
+      <Typography variant="h3" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+      <span style={{ color: '#f7efef' }}>Watch</span>
+        <span style={{ color: "#FC1503" }}>Now</span>
+      </Typography>
+    </Box>
     <SearchBar />
   </Stack>
 );
